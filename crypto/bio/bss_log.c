66c4e1483a43ebbb9495587e46dd1f3f08e7114b
rdos314 <leif@rdos.net>
2025-04-30 15:43:09 +0200

    Fixed problems in bss functions.

--- crypto/bio/bss_log.c
+++ crypto/bio/bss_log.c
@@ -43,6 +43,8 @@ void *_malloc32(__size_t);
                                  * _ANSI_C_SOURCE */
 #elif defined(__DJGPP__) && defined(OPENSSL_NO_SOCK)
 # define NO_SYSLOG
+#elif defined(OPENSSL_SYS_RDOS)
+# define NO_SYSLOG
 #elif (!defined(MSDOS) || defined(WATT32)) && !defined(OPENSSL_SYS_VXWORKS) && !defined(NO_SYSLOG)
 # include <syslog.h>
 #endif
